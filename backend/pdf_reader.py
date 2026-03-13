c65190c9d0ae938c5e8276c2d4a201f7de53c56f
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2026-03-14 01:13:17 +0200

    finish the setup for the images creator

--- backend/pdf_reader.py
+++ backend/pdf_reader.py
@@ -1,18 +1,25 @@
 import fitz
+import os
 
-pdf_path = "Folheto_Digital_Bons_Negcios.pdf"
 
-doc = fitz.open(pdf_path)
+def remove_img_from_pdf(pdf_path):
+    # Subir um diretório e criar result/imgs
+    parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    result_dir = os.path.join(parent_dir, "result")
+    os.makedirs(result_dir, exist_ok=True)
+    imgs_dir = os.path.join(result_dir, "imgs")
+    os.makedirs(imgs_dir, exist_ok=True)
 
-for page_number in range(len(doc)):
-    page = doc.load_page(page_number)
+    # Abrir PDF
+    doc = fitz.open(pdf_path)
+    for page_number in range(len(doc)):
+        page = doc.load_page(page_number)
+        pix = page.get_pixmap(dpi=300)
 
-    pix = page.get_pixmap(dpi=300)
+        # Salvar imagem numerada
+        output = os.path.join(imgs_dir, f"pagina_{page_number + 1}.jpg")
+        pix.save(output)
+        print("Guardado:", output)
 
-    output = f"imgs\pagina_{page_number + 1}.jpg"
-
-    pix.save(output)
-
-    print("Guardado:", output)
-
-doc.close()
+    doc.close()
+remove_img_from_pdf(r"C:\Users\Utilizador\Desktop\Ideias para negocios\ler_panfletos\backend\Folheto_Digital_Bons_Negcios.pdf")
